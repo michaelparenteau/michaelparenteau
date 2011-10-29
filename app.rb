514ad453ce2a50a69daef3a09ffbce0fbb990e17
Michael Parenteau <prmparenteau@aol.com>
2011-10-29 16:08:11 -0400

    getting tweet logic out of the view

--- app.rb
+++ app.rb
@@ -17,6 +17,7 @@ set :haml, {:format => :html5}
 get '/' do
 
   @page_title = "Artist & Designer at Relevance, Inc. in Durham, NC"
+  @tweet = Twitter.user_timeline("parenteau").first.text
   @player = Dribbble::Player.find('michaelparenteau')
   @shot = @player.shots.first
 
